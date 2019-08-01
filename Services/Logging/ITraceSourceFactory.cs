e8c024d57c2f1fbe21a1d12a1c481f153d44dd09
wim van laer <van.laer.wim@gmail.com>
2019-08-01 18:17:15 +0200

    Added trace factory and listeners

---
+++ Services/Logging/ITraceSourceFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace MyCSharpLib.Services.Logging
+{
+    public interface ITraceSourceFactory
+    {
+        TraceSource Resolve(string name, SourceLevels defaultLevel = SourceLevels.All);
+
+        ITraceSourceFactory RegisterListener<T>() where T : ITraceListener;
+        ITraceSourceFactory RegisterListener<T>(T listener) where T : ITraceListener;
+        ITraceSourceFactory RegisterListener(TraceListener listener);
+    }
+}
