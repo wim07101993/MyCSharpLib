aa11f5a63258384afa3d3ffe35270892723cc793
wim van laer <van.laer.wim@gmail.com>
2019-08-04 14:15:48 +0200

    Added disposable tests

---
+++ MyCSharpLib.Tests/Mocks/FailingDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCSharpLib.Tests.Mocks
+{
+    public class FailingDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyCSharpLib.Tests/Mocks/SuccedingDisposable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyCSharpLib.Tests.Mocks
+{
+    public class SuccedingDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
