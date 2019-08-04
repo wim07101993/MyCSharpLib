9c36cccc64b0b680c989d6e6f7c03dd422b96a57
wim van laer <van.laer.wim@gmail.com>
2019-08-04 22:07:15 +0200

    Added ToAsciiBytes test

---
+++ MyCSharpLib.Tests/Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using MyCSharpLib.Extensions;
+using FluentAssertions;
+
+namespace MyCSharpLib.Tests.Tests.Extensions
+{
+    public class StringExtensions
+    {
+        [Test]
+        public void ToAsciiBytes()
+        {
+            "hello world"
+                .ToAsciiBytes()
+                .Should()
+                .BeEquivalentTo(new byte[] { 104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100 }, "that is the ascii representation.");
+        }
+    }
+}
