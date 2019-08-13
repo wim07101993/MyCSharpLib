e9f77ef0a19c634df6cbd67308390ded0f6d24ed
wim <van.laer.wim@gmail.com>
2019-08-13 22:53:38 +0200

    Created ObjectBroser control (no style) and dependen classes

---
+++ MyCSharpLib.Wpf/Controls/ObjectBrowser/ObjectBrowserDataTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MyCSharpLib.Wpf.Controls.ObjectBrowser
+{
+    public class ObjectBrowserDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate StringTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is string)
+                return StringTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
---
+++ MyCSharpLib/Reflection/AuthorizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyCSharpLib.Reflection
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = true)]
+    sealed class AuthorizeAttribute : Attribute
+    {
+        public AuthorizeAttribute(int authorizedLevels)
+        {
+            AuthorizedLevels = authorizedLevels;
+        }
+
+        public int AuthorizedLevels { get; }
+    }
+}
