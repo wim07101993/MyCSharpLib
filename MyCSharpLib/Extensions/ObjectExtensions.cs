ae96098fa2470246c1b56af7d1d45a3cd9942420
wim van laer <van.laer.wim@gmail.com>
2019-09-16 12:10:54 +0200

    Added cast object method

--- MyCSharpLib/Extensions/ObjectExtensions.cs
+++ MyCSharpLib/Extensions/ObjectExtensions.cs
@@ -10,5 +10,16 @@ namespace MyCSharpLib.Extensions
                 return obj;
             return Convert.ChangeType(obj, type);
         }
+
+        public static T CastObject<T>(this object obj)
+        {
+            if (obj == null)
+                return default;
+
+            if (obj.GetType() == typeof(T))
+                return (T)obj;
+
+            return (T)Convert.ChangeType(obj, typeof(T));
+        }
     }
 }
