a09b715188e9ef65456df6a84160982cf83edfa0
wim van laer <van.laer.wim@gmail.com>
2019-10-05 11:21:06 +0200

    feat/validatable: Created Validatable

---
+++ WSharp.Tests/Mocks/ValidationObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSharp.Tests.Mocks
+{
+    public class ValidationObject
+    {
+        [Range(5, int.MaxValue)]
+        public int GreaterThen5 { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string NotEmpty { get; set; }
+
+        [Required]
+        public object NotNull { get; set; }
+
+        public NestedValidationObject Nested { get; set; }
+
+        public List<NestedValidationObject> List { get; set; }
+    }
+}
