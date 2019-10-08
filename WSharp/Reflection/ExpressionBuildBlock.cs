22a9fb2c6772ded14ea5e31b753e938bc6e7296d
wim van laer <van.laer.wim@gmail.com>
2019-10-08 22:41:42 +0200

    feat/select-expression-builder: added enumerable search

--- WSharp/Reflection/ExpressionBuildBlock.cs
+++ WSharp/Reflection/ExpressionBuildBlock.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using WSharp.Extensions;
 
 namespace WSharp.Reflection
@@ -36,69 +37,154 @@ namespace WSharp.Reflection
         public PropertyInfo IndexProperty { get; set; }
         public ParameterInfo Parameter { get; set; }
         public object ParameterValue { get; set; }
+        public PropertyInfo GenericIdProperty { get; set; }
+        public MethodInfo GenericIdMethod { get; set; }
 
 
         private void BuildNextBlock()
         {
             var section = Section;
-            Property = GetProperty(section);
-            if (Property != null)
+            if (TryGetProperty(section))
             {
                 NextBlock = new ExpressionBuildBlock(Property.PropertyType, Sections.Skip(1));
                 return;
             }
-
-            Method = GetMethod(section);
-            if (Method != null)
+            
+            if (TryGetMethod(section))
             {
                 NextBlock = new ExpressionBuildBlock(Method.ReturnType, Sections.Skip(1));
                 return;
             }
-
-            IndexProperty = GetIndexProperty();
-            if (IndexProperty != null)
+            
+            if (TryGetIndexProperty() && TryGetIndexParameter(section))
             {
-                Parameter = IndexProperty.GetIndexParameters().First();
-                try
-                {
-                    ParameterValue = Convert.ChangeType(section, Parameter.ParameterType);
-                    NextBlock = new ExpressionBuildBlock(IndexProperty.PropertyType, Sections.Skip(1));
-                    return;
-                }
-                catch (Exception)
-                {
-                }
+                NextBlock = new ExpressionBuildBlock(IndexProperty.PropertyType, Sections.Skip(1));
+                return;
             }
 
-            if (typeof(IEnumerable).IsAssignableFrom(ParentType))
+            if (TryGetGenericIdMember())
             {
-
+                NextBlock = new ExpressionBuildBlock(GenericIdProperty?.PropertyType ?? GenericIdMethod.ReturnType, Sections.Skip(1));
+                return;
             }
         }
 
-        public PropertyInfo GetProperty(string name)
-           => IncludeProperties
-           ? ParentType.GetProperties().FirstOrDefault(x => CompareName(x, name))
-           : null;
+        private bool TryGetProperty(string name)
+        {
+            if (!IncludeProperties)
+                return false;
 
-        public MethodInfo GetMethod(string name)
-            => IncludeMethods
-            ? ParentType.GetMethods()
+            Property = ParentType.GetProperties()
+                .FirstOrDefault(x => CompareName(x, name));
+
+            return Property != null;
+        }
+
+        private bool TryGetMethod(string name)
+        {
+            if (!IncludeMethods)
+                return false;
+
+            Method = ParentType.GetMethods()
                 .Where(x =>
                 {
                     var parameters = x.GetParameters();
                     return (parameters.Length == 0 || parameters.All(p => p.HasDefaultValue)) &&
                         x.ReturnParameter != null;
                 })
-                .FirstOrDefault(x => CompareName(x, name))
-            : null;
+                .FirstOrDefault(x => CompareName(x, name));
 
-        public PropertyInfo GetIndexProperty()
-            => IncludeIndexProperties
-            ? ParentType.GetProperties().FirstOrDefault(x => x.GetIndexParameters().Length > 0)
-            : null;
+            return Method != null;
+        }
 
-        protected bool CompareName(MemberInfo member, string name)
+        private bool TryGetIndexProperty()
+        {
+            if (!IncludeIndexProperties)
+                return false;
+
+            IndexProperty = ParentType.GetProperties()
+                .FirstOrDefault(x => x.GetIndexParameters().Length > 0);
+
+            return IndexProperty != null;
+        }
+
+        private bool TryGetIndexParameter(string value)
+        {
+            Parameter = IndexProperty.GetIndexParameters().First();
+            try
+            {
+                ParameterValue = Convert.ChangeType(value, Parameter.ParameterType);
+                return true;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
+        } 
+
+        private bool TryGetGenericIdMember()
+        {
+            if (!typeof(IEnumerable<>).IsAssignableFrom(ParentType))
+                return false;
+
+            var genericType = GetGenericArgumentType();
+            var genericProperties = genericType.GetProperties();
+
+            var idRegex = new Regex(Regexamples.IdRegex);
+            if (TryGetGenericIdProperty(genericProperties, idRegex))
+                return true;
+
+            var nameRegex = new Regex(Regexamples.NameRegex);
+            if (TryGetGenericIdProperty(genericProperties, nameRegex))
+                return true;
+
+            var genericMethods = genericType.GetMethods()
+                .Where(x => x.ReturnType != null && x.GetParameters().Length == 0)
+                .ToArray();
+
+            if (TryGetGenericIdMethod(genericMethods, idRegex))
+                return true;
+            if (TryGetGenericIdMethod(genericMethods, nameRegex))
+                return true;
+
+            return false;
+        }
+
+        private bool TryGetGenericIdProperty(PropertyInfo[] properties, Regex regex)
+        {
+            GenericIdProperty = properties.FirstOrDefault(x =>
+            {
+                if (AcceptDisplayName && regex.IsMatch(x.GetDisplayName()))
+                    return true;
+                return regex.IsMatch(x.Name);
+            });
+
+            return GenericIdProperty != null;
+        }
+
+        private bool TryGetGenericIdMethod(MethodInfo[] methods, Regex regex)
+        {
+            GenericIdMethod = methods.FirstOrDefault(x =>
+            {
+                if (AcceptDisplayName && regex.IsMatch(x.GetDisplayName()))
+                    return true;
+                return regex.IsMatch(x.Name);
+            });
+
+            return GenericIdProperty != null;
+        }
+
+        private Type GetGenericArgumentType()
+        {
+            var type = ParentType;
+
+            while (type != typeof(IEnumerable<>))
+                type = type.GetInterfaces().First(x => typeof(IEnumerable<>).IsAssignableFrom(ParentType));
+
+            return type.GetGenericArguments().First();
+        }
+
+        private bool CompareName(MemberInfo member, string name)
         {
             var comparisonType = CaseSensitive
                 ? StringComparison.InvariantCulture
--- WSharp/Regexamples.cs
+++ WSharp/Regexamples.cs
@@ -3,5 +3,7 @@
     public static class Regexamples
     {
         public const string IpAddressRegex = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+        public const string IdRegex = @".*I[D,d]$";
+        public const string NameRegex = @".*Name$";
     }
 }
