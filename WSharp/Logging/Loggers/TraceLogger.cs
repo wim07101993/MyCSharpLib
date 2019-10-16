14303986e137d830cad4ae3ce6a04f0467215d85
wim van laer <van.laer.wim@gmail.com>
2019-10-16 20:50:13 +0200

    feat/logging: added more loggers

---
+++ WSharp/Logging/Loggers/TraceLogger.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace WSharp.Logging.Loggers
+{
+    public class TraceLogger : ALogger, ITraceLogger
+    {
+        public TraceLogger()
+        {
+        }
+
+        public override void InternalLog(ILogEntry logEntry) => Trace.Write(logEntry);
+    }
+}
