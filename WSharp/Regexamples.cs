10395717af1eb4fcc974f6b8152dbcaf9817a2cd
Wim Van Laer <wim07101993@users.noreply.github.com>
2020-05-12 10:59:36 +0200

    Updated regex to match an IP address
    ^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$

--- WSharp/Regexamples.cs
+++ WSharp/Regexamples.cs
@@ -4,6 +4,6 @@
     public static class Regexamples
     {
         /// <summary>Regex string that filters IP Addresess</summary>
-        public const string IpAddressRegex = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+        public const string IpAddressRegex = @"^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$";
     }
 }
