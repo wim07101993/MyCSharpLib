1f84bc6c2299e236128b5a82076ff3732d9d48ea
wim07101993 <van.laer.wim@gmail.com>
2020-08-29 20:09:57 +0200

    feat/dialog-service: created dialog options

---
+++ WSharp/Dialogs/Options/ErrorDialogOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WSharp.Dialogs.Options
+{
+    public struct ErrorDialogOptions : IErrorDialogOptions
+    {
+        public ErrorDialogOptions(
+            string title, string message, bool showException, Exception exception)
+        {
+            Title = title;
+            Message = message;
+            ShowException = showException;
+            Exception = exception;
+        }
+
+        public string Title { get; }
+
+        public string Message { get; }
+
+        public bool ShowException { get; }
+
+        public Exception Exception { get; }
+    }
+}
