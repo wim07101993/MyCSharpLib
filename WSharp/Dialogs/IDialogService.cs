ce8dbf70ef400209ca5863393b2995c1e3504f87
wim07101993 <van.laer.wim@gmail.com>
2020-08-29 20:29:56 +0200

    feat/dialog-service: created IDialogService

---
+++ WSharp/Dialogs/IDialogService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using WSharp.Dialogs.Options;
+using WSharp.Dialogs.Results;
+
+namespace WSharp.Dialogs
+{
+    public interface IDialogService
+    {
+        IErrorDialogResult ShowErrorDialog(IErrorDialogOptions options);
+
+        IOpenFileDialogResult ShowOpenFileDialog(IOpenFileDialogOptions options);
+
+        IProgressDialogResult ShowProgressDialog(IProgressDialogOptions options);
+
+        ISaveFileDialogResult ShowSaveFileDialog(ISaveFileDialogOptions options);
+
+        ISimpleDialogResult ShowSimpleDialog(ISimpleDialogOptions options);
+
+        Task<ISaveFileDialogResult> ShowSaveFileDialogAsync(ISaveFileDialogOptions options);
+
+        Task<IProgressDialogResult> ShowProgressDialogAsync(IProgressDialogOptions options);
+
+        Task<IOpenFileDialogResult> ShowOpenFileDialogAsync(IOpenFileDialogOptions options);
+
+        Task<IErrorDialogResult> ShowErrorDialogAsync(IErrorDialogOptions options);
+
+        Task<ISimpleDialogResult> ShowSimpleDialogAsync(ISimpleDialogOptions options);
+    }
+}
